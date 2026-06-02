0792cbc10b6d0f150bacdcc2c4700ff4d6a8fa32
CoolorVi <lizavetaaaaaaaaa@yandex.ru>
2026-06-02 15:06:42 +0300

    feat: complete dynamic

--- src/modules/dynamic.js
+++ src/modules/dynamic.js
@@ -4,6 +4,18 @@ const dynamic = async () => {
   // Dynamically import plugin from plugins/ directory
   // Call run() function and print result
   // Handle missing plugin case
+  const arg = process.argv.slice(2)
+
+  let plugin = ""
+
+  try {
+    plugin = await import(`../modules/plugins/${arg[0]}.js`)
+  } catch {
+    console.log("Plugin not found")
+    process.exit(1)
+  }
+
+  console.log(plugin.run())
 };
 
 await dynamic();
